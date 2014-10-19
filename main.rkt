f72410f0ec5a0364d128eba26ae04b0bf64999dd
Juan Ibiapina <juanibiapina@gmail.com>
2014-10-19 18:39:13 -0200

    Add a runner for marco files.

---
+++ main.rkt
@@ -0,0 +1,23 @@
+#lang racket
+
+(require
+  (prefix-in m: "language/main.rkt")
+  "loader.rkt")
+
+(define filename
+  (command-line
+    #:args (filename)
+    filename))
+
+(call-with-input-file
+  filename
+  (lambda (port)
+    (eval-with-bindings
+      port
+      (cons
+        "module-path"
+        (m:string
+          (path->string
+            (let-values ([(path name _) (split-path (collection-file-path "main.rkt" "marco"))])
+              (build-path path "modules"))))))
+    (void)))
